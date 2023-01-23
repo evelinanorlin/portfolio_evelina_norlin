2dcab9b2eb84688187f1478471a38089401a82e1
Evelina Norlin <evelina.norlin@hotmail.com>
2023-01-23 18:18:16 +0100

    added menu toggle

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 import './style.scss';
+
+const burgerBtn = document.getElementById('burger');
+const navLinks = document.getElementById('nav-links');
+let menuOpen = false;
+
+
+
+//menu-toggle
+function menuToggle() {
+
+  console.log('hello');
+  
+  if (!menuOpen) {
+    navLinks.style.display = 'block';
+    burgerBtn.classList.add('open');
+    //gsap.to('.mobile-menu', { duration: 0.5, left: '0' });
+    menuOpen = true;
+  } else {
+    //gsap.to('.mobile-menu', { duration: 0.5, left: '1000px', onComplete: hideMenu });
+    navLinks.style.display = 'none';
+    burgerBtn.classList.remove('open');
+    menuOpen = false;
+  }
+}
+
+burgerBtn.addEventListener('click', menuToggle);
