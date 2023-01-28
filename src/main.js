8047ee4237b91d3432641c210e3a5b97f296a11b
Evelina Norlin <evelina.norlin@hotmail.com>
2023-01-28 14:51:13 +0100

    added some animations to text

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './style.scss';
+import { gsap } from "gsap";
 
 const burgerBtn = document.getElementById('burger');
 const navLinks = document.getElementById('nav-links');
 let menuOpen = false;
 
 
+//code for navigaion
 
 //menu-toggle
 function menuToggle() {
@@ -12,16 +14,49 @@ function menuToggle() {
   console.log('hello');
   
   if (!menuOpen) {
-    navLinks.style.display = 'block';
+    // navLinks.style.display = 'block';
     burgerBtn.classList.add('open');
     //gsap.to('.mobile-menu', { duration: 0.5, left: '0' });
+    gsap.to(".nav-links", { right: 0, duration: 0.5 })
     menuOpen = true;
   } else {
     //gsap.to('.mobile-menu', { duration: 0.5, left: '1000px', onComplete: hideMenu });
-    navLinks.style.display = 'none';
+    // navLinks.style.display = 'none';
     burgerBtn.classList.remove('open');
+    gsap.to(".nav-links", { right: -2000, duration: 0 })
     menuOpen = false;
   }
 }
 
 burgerBtn.addEventListener('click', menuToggle);
+
+// textanimations
+
+const animateParagraph = document.querySelectorAll('.slide-in');
+const animateParagraph2 = document.querySelectorAll('.slide-in2');
+
+const textObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gsap.to('.slide-in', { opacity: 1, duration: 4})
+      console.log('hurra')
+    }
+  });
+});
+
+const textObserver2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gsap.to('.slide-in2', { opacity: 1, duration: 4})
+      console.log('hurra')
+    }
+  });
+});
+
+animateParagraph.forEach((element) => {
+  textObserver.observe(element);
+})
+
+animateParagraph2.forEach((element) => {
+  textObserver2.observe(element);
+})
