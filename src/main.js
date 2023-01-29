7f93c5fffac4ed34d23dedbbf2252ca19ff344ae
Evelina Norlin <evelina.norlin@hotmail.com>
2023-01-29 15:27:45 +0100

    css and js fix

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import './style.scss';
 import { gsap } from "gsap";
+import { portfolioItems } from './portfolio_items';
 
 const burgerBtn = document.getElementById('burger');
-const navLinks = document.getElementById('nav-links');
+const navLink = document.querySelectorAll('.nav-link');
+const portfolioContainer = document.getElementById('portfolio-container');
 let menuOpen = false;
 
 
@@ -10,36 +12,79 @@ let menuOpen = false;
 
 //menu-toggle
 function menuToggle() {
-
-  console.log('hello');
   
   if (!menuOpen) {
-    // navLinks.style.display = 'block';
     burgerBtn.classList.add('open');
-    //gsap.to('.mobile-menu', { duration: 0.5, left: '0' });
     gsap.to(".nav-links", { right: 0, duration: 0.5 })
     menuOpen = true;
   } else {
-    //gsap.to('.mobile-menu', { duration: 0.5, left: '1000px', onComplete: hideMenu });
-    // navLinks.style.display = 'none';
     burgerBtn.classList.remove('open');
     gsap.to(".nav-links", { right: -2000, duration: 0 })
     menuOpen = false;
   }
 }
 
+
+// Closes menu when link is clicked
+function linkFunction(link){
+  link.addEventListener('click', menuToggle);
+}
+
 burgerBtn.addEventListener('click', menuToggle);
+navLink.forEach(linkFunction);
+
+//add active class to menu links
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    this.className += " active";
+  });
+}
+
+// Portfolio
+
+for (let i = 0; i < portfolioItems.length; i++) {
+    portfolioContainer.innerHTML += `
+    <div class="img-hover-zoom">
+      <img
+        src="src/images/${portfolioItems[i].img}"
+        alt="picture of a yoga website landing"
+        height="200"
+        width="250"
+        loading="lazy"
+      />
+      <div class="link-container">
+        <a href="#">See more</a>
+      </div>
+    </div>
+    `
+}
 
 // textanimations
 
-const animateParagraph = document.querySelectorAll('.slide-in');
+const animateParagraph1 = document.querySelectorAll('.slide-in1');
 const animateParagraph2 = document.querySelectorAll('.slide-in2');
+const animateParagraph3 = document.querySelectorAll('.slide-in3');
+const animateParagraph4 = document.querySelectorAll('.slide-in4');
+const animateParagraph5 = document.querySelectorAll('.slide-in5');
+const animateParagraph6 = document.querySelectorAll('.slide-in6');
+const animateParagraph7 = document.querySelectorAll('.slide-in7');
+const animateParagraph8 = document.querySelectorAll('.slide-in8');
+const animateParagraph9 = document.querySelectorAll('.slide-in9');
+const animateParagraph10 = document.querySelectorAll('.slide-in10');
 
-const textObserver = new IntersectionObserver((entries) => {
+const textObserver1 = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      gsap.to('.slide-in', { opacity: 1, duration: 4})
-      console.log('hurra')
+      gsap.to('.slide-in1', { opacity: 1, duration: 4, delay: 0.3});
+    }
+    else {
+      gsap.to('.slide-in1', { opacity: 0, duration: 4, delay: 0.3});
     }
   });
 });
@@ -47,16 +92,137 @@ const textObserver = new IntersectionObserver((entries) => {
 const textObserver2 = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      gsap.to('.slide-in2', { opacity: 1, duration: 4})
+      gsap.to('.slide-in2', { opacity: 1, duration: 4, delay: 0.3})
+    }     else {
+      gsap.to('.slide-in2', { opacity: 0, duration: 4, delay: 0.3});
+    }
+  });
+  });
+
+const textObserver3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gsap.to('.slide-in3', { opacity: 1, duration: 4, delay: 0.3})
       console.log('hurra')
+    }     
+    else {
+      gsap.to('.slide-in3', { opacity: 0, duration: 4, delay: 0.3});
     }
   });
 });
 
-animateParagraph.forEach((element) => {
-  textObserver.observe(element);
+const textObserver4 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gsap.to('.slide-in4', { opacity: 1, duration: 4, delay: 0.3})
+    } 
+    else {
+      gsap.to('.slide-in4', { opacity: 0, duration: 4, delay: 0.3});
+    }
+  });
+});
+
+const textObserver5 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gsap.to('.slide-in5', { opacity: 1, duration: 4, delay: 0.3})
+    }
+    else {
+      gsap.to('.slide-in5', { opacity: 0, duration: 4, delay: 0.3});
+    }
+  });
+});
+
+const textObserver6 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gsap.to('.slide-in6', { opacity: 1, duration: 4, delay: 0.3})
+    }
+    else {
+      gsap.to('.slide-in6', { opacity: 0, duration: 4, delay: 0.3});
+    }
+  });
+});
+
+const textObserver7 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gsap.to('.slide-in7', { opacity: 1, duration: 4, delay: 0.3})
+    }
+    else {
+      gsap.to('.slide-in7', { opacity: 0, duration: 4, delay: 0.3});
+    }
+  });
+});
+
+const textObserver8 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gsap.to('.slide-in8', { opacity: 1, duration: 4, delay: 0.3})
+    }
+    else {
+      gsap.to('.slide-in8', { opacity: 0, duration: 4, delay: 0.3});
+    }
+  });
+});
+
+const textObserver9 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gsap.to('.slide-in9', { opacity: 1, duration: 4, delay: 0.3})
+    }
+    else {
+      gsap.to('.slide-in9', { opacity: 0, duration: 4, delay: 0.3});
+    }
+  });
+});
+
+const textObserver10 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      gsap.to('.slide-in10', { opacity: 1, duration: 4, delay: 0.3})
+    }
+    else {
+      gsap.to('.slide-in10', { opacity: 0, duration: 4, delay: 0.3});
+    }
+  });
+});
+
+animateParagraph1.forEach((element) => {
+  textObserver1.observe(element);
 })
 
 animateParagraph2.forEach((element) => {
   textObserver2.observe(element);
+})
+
+animateParagraph3.forEach((element) => {
+  textObserver3.observe(element);
+})
+
+animateParagraph4.forEach((element) => {
+  textObserver4.observe(element);
+})
+animateParagraph5.forEach((element) => {
+  textObserver5.observe(element);
+})
+
+animateParagraph6.forEach((element) => {
+  textObserver6.observe(element);
+})
+
+animateParagraph7.forEach((element) => {
+  textObserver7.observe(element);
+})
+
+animateParagraph8.forEach((element) => {
+  textObserver8.observe(element);
+})
+
+animateParagraph9.forEach((element) => {
+  textObserver9.observe(element);
+})
+
+animateParagraph10.forEach((element) => {
+  textObserver10.observe(element);
 })
