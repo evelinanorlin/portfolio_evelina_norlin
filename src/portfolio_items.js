978519b887c8b93476a4acc7c7f0e19cb85a5456
Evelina Norlin <evelina.norlin@hotmail.com>
2023-01-30 13:52:43 +0100

    portfoliofixes

---
+++ src/portfolio_items.js
@@ -0,0 +1,13 @@
+export const portfolioItems = [
+{ 
+  name: 'Norlin movement',
+  img: 'norlin_movement.png',
+},
+{
+  name: 'toTheMoon',
+  img: 'to_the_moon.png',
+},
+{ 
+  name: 'quany firefly',
+  img: 'quant_firefly.png',
+}]
