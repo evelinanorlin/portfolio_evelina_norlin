3d2993cc8c62f7e6456c4b48587345d8f51e2eb4
Evelina Norlin <evelina.norlin@hotmail.com>
2023-02-03 12:45:37 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from "vite";
 
-export default({
-  base: '/portfolio_evelina_norlin/'
-});
+
+export default defineConfig({
+  base: '/portfolio_evelina_norlin/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
+})
